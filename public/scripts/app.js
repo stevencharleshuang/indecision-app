4eecbbf7e783333d3919fa229d10ece6fef7c83d
Steven Huang <steven.charles.huang@gmail.com>
2018-07-17 23:01:20 -0400

    integrated babel-cli and presets: env,react

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,13 @@
+'use strict';
+
 console.log('app.js is running');
 
 // Babel Compiled Code
-let template = React.createElement(
-    "h1",
-    { id: "some-id" },
-    "This is JSX from app.js!"
-  );
-let appRoot = document.getElementById('app');
+var template = React.createElement(
+  'h1',
+  null,
+  'Indecision App'
+);
+var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+console.log('app.js is running');
+
+// Babel Compiled Code
+let template = <h1>Indecision App</h1>
+let appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
