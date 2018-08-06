42bd30da031a2c8dd78b0d713bed3468c8fb8966
Steven Huang <steven.charles.huang@gmail.com>
2018-08-06 00:38:35 -0400

    added styles to modal

--- src/components/OptionModal.jsx
+++ src/components/OptionModal.jsx
@@ -11,10 +11,12 @@ export default class OptionModal extends React.Component {
         isOpen={!!this.props.selectedOption}
         onRequestClose={this.props.handleClearModal}
         contentLabel="Selected Option"
+        closeTimeoutMS={200}
+        className="modal"
       >
-        <h3>Selected Option</h3>
-        {this.props.selectedOption && <p>{this.props.selectedOption}</p>}
-        <button onClick={this.props.handleClearModal}>Okay</button>
+        <h3 className="modal__title">Selected Option</h3>
+        {this.props.selectedOption && <p className="modal__body">{this.props.selectedOption}</p>}
+        <button className="button" onClick={this.props.handleClearModal}>Okay</button>
       </Modal>
     );
   }
